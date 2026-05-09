bfc2bba425b1ab38d72074ea29496a5a4755a4f4
Syumaylah <syumaylah1@gmail.com>
2026-05-09 20:05:26 +0700

    update database testi

--- database/migrations/2026_05_09_125744_create_testimonials_table.php
+++ database/migrations/2026_05_09_125744_create_testimonials_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('testimonials', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('nama');
             $table->string('peran')->nullable();
             $table->text('pesan');
